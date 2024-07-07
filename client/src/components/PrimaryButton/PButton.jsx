d44737ca2a6f358d672331102b19883c89b1cfce
Deshitha Gallage <deshithagallage623@gmail.com>
2024-07-08 01:30:14 +0530

    Added tailwind to the project

--- client/src/components/PrimaryButton/PButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PButton = ({text, onClickFunc}) => {
-  return <button onClick={onClickFunc} className="w-full h-full mt-4 bg-blue-500 text-white rounded-md drop-shadow-xl tracking-widest flex justify-center items-center hover:-translate-y-1 hover:bg-blue-600 hover:shadow-lg transition-transform duration-300">
-  {text}
-</button>
-}
-
-export default PButton
