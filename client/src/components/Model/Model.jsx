0fe5c9f614f6cd3c8aaa3e371f234b07d250f34e
Deshitha Gallage <deshithagallage623@gmail.com>
2024-08-12 22:31:22 +0530

    New updates with bug fixes

--- client/src/components/Model/Model.jsx
+++ client/src/components/Model/Model.jsx
@@ -1,16 +1,18 @@
+//modal
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import PrimaryButton from "../PrimaryButton/PrimaryButton";
 
-const Modal = ({ content, overlayClick }) => {
+const Modal = ({ content, overlayClick, bgColor = "bg-black" }) => {
   const navigate = useNavigate();
 
   return (
     <div
       onClick={overlayClick}
-      className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 backdrop-blur-sm flex justify-center items-center z-10"
+      className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-70 backdrop-blur-sm flex justify-center items-center z-10`}
     >
-      <div className="bg-black p-5 text-center text-white rounded-lg">
+      <div className={`${bgColor} p-5 text-center text-white rounded-lg`}>
         {content}
       </div>
     </div>
--- client/src/pages/Dashboard/Center/DoctorManagement.jsx
+++ client/src/pages/Dashboard/Center/DoctorManagement.jsx
@@ -1,54 +1,108 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+// DoctorManagement
+
+import React, { useEffect, useState } from "react";
 import CenterSidebar from "../Sidebar/CenterSidebar.jsx";
-import styles from "./DoctorManagement.module.css";
 import Navbar from "../../../components/Navbar/Navbar.jsx";
+import axios from "axios";
+import Cookies from "js-cookie";
+import PrimaryButton from "../../../components/PrimaryButton/PrimaryButton.jsx";
+import AddDoctorModal from "../../../components/Model/AddDoctorModal.jsx";
+import EditDoctorModal from "../../../components/Model/EditDoctorModal.jsx";
+import DoctorCard from "./DoctorCard.jsx";
 
 const DoctorManagement = () => {
-  const navigate = useNavigate();
+  const [doctors, setDoctors] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [editingDoctor, setEditingDoctor] = useState({});
 
-  const handleEditDoctor = (doctorId) => {
-    navigate(`/dashboard/center/doctors/edit/${doctorId}`);
+  const refreshDoctors = () => {
+    axios
+      .get("http://localhost:3000/api/centers/doctors", {
+        headers: {
+          "x-auth-token": Cookies.get("token"),
+        },
+      })
+      .then((res) => {
+        setDoctors(res.data);
+      })
+      .catch((err) => {
+        if (err.response && err.response.status === 401) {
+          Cookies.remove("token");
+          Cookies.remove("userRole");
+          Cookies.remove("userId");
+          console.log(err.response.status);
+          window.location.reload();
+        } else {
+          console.log(err);
+        }
+      });
   };
 
-  const handleAddDoctor = () => {
-    navigate(`/dashboard/center/doctors/new`);
-  };
-
-  // Dummy doctor data
-  const doctors = [
-    { id: 1, name: "Dr. John Doe", timeslots: ["9am - 12pm", "2pm - 5pm"] },
-    { id: 2, name: "Dr. Jane Smith", timeslots: ["10am - 1pm", "3pm - 6pm"] },
-  ];
+  useEffect(() => {
+    refreshDoctors();
+  }, []);
 
   return (
     <div>
       <Navbar />
-      <div className={styles.doctorManagement}>
-        <CenterSidebar />
-        <div className={styles.content}>
-          <h1>Doctor Management</h1>
-          <div className={styles.doctorList}>
-            {doctors.map((doctor) => (
-              <div key={doctor.id} className={styles.doctorCard}>
-                <h2>{doctor.name}</h2>
-                <p>Timeslots:</p>
-                <ul>
-                  {doctor.timeslots.map((slot, index) => (
-                    <li key={index}>{slot}</li>
-                  ))}
-                </ul>
-                <button onClick={() => handleEditDoctor(doctor.id)}>
-                  Edit
-                </button>
-              </div>
-            ))}
+      <div className="flex w-full h-full">
+        <div className="w-[17%] h-full">
+          <CenterSidebar />
+        </div>
+        <div className="w-[83%] h-full my-24 flex flex-col justify-center items-center">
+          <h1 className="text-4xl font-bold mb-8">Doctor Management</h1>
+          <div className="w-full px-16 flex flex-col justify-center items-start">
+            <h2 className="text-2xl font-bold text-start">
+              Register a New Doctor
+            </h2>
+            <div className="w-full h-1 bg-gray-200 mt-1 mb-3"></div>
+            <div className="h-12 mb-5">
+              <PrimaryButton
+                text="Register New Doctor"
+                onClick={() => setShowModal(true)}
+                className="p-6"
+                removeTranslate={true}
+              />
+            </div>
+
+            <h2 className="text-2xl font-bold text-start">
+              Registered Doctors
+            </h2>
+            <div className="w-full h-1 bg-gray-200 mt-1 mb-3"></div>
+            <div className="w-full grid grid-cols-2 gap-x-6 gap-y-2">
+              {doctors.length > 0 ? (
+                doctors.map((doctor) => (
+                  <DoctorCard
+                    doctor={doctor}
+                    handleEditDoctor={() => {
+                      setEditingDoctor(doctor);
+                      setShowEditModal(true);
+                    }}
+                    handleEditTimeSlots={() => console.log("Edit Time Slots")}
+                  />
+                ))
+              ) : (
+                <h1 className="text-lg font-bold">No Doctors Found</h1>
+              )}
+            </div>
           </div>
-          <button className={styles.addDoctor} onClick={handleAddDoctor}>
-            Add New Doctor
-          </button>
         </div>
       </div>
+      <AddDoctorModal
+        showModal={showModal}
+        handleClose={() => setShowModal(false)}
+        refreshDoctors={refreshDoctors}
+      />
+      <EditDoctorModal
+        formData={editingDoctor}
+        showModal={showEditModal}
+        handleClose={() => {
+          setEditingDoctor({});
+          setShowEditModal(false);
+        }}
+        refreshDoctors={refreshDoctors}
+      />
     </div>
   );
 };
